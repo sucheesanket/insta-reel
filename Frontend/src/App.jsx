3d46d5bd07e48f9691b2950f107247c9c3e39029
sucheesanket <167225590+sucheesanket@users.noreply.github.com>
2026-03-20 18:29:31 +0530

    not doing anything coming for college then do some code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import AppRoutes from "./AppRoutes"
+import './style.scss'
+
+
+const App = () => {
+  return (
+    <div>
+      <AppRoutes/>
+    </div>
+  )
+}
+
+export default App
